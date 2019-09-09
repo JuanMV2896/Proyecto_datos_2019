8105cf6671a8cc0fa56e60feb318d3064b2cb6d5
hostname2 <44320487+hostname2@users.noreply.github.com>
2019-09-09 15:53:31 -0600

    hola mundo

---
+++ Proy_Datos_1/Stack.h
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Stack.h
+ * Author: sebas
+ *
+ * Created on 9 de septiembre de 2019, 03:50 PM
+ */
+
+#ifndef STACK_H
+#define STACK_H
+
+class Stack {
+public:
+    Stack();
+    Stack(const Stack& orig);
+    virtual ~Stack();
+    virtual void metdo1();
+private:
+
+};
+
+#endif /* STACK_H */
+
---
+++ Proy_Datos_1/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: sebas
+ *
+ * Created on 9 de septiembre de 2019, 03:47 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hola Mundo";
+    return 0;
+}
+
